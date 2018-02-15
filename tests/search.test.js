1f7b90a026f3f3ddd373bc50ac809b9a44c80969
Nicolò Pomini <nicolo.pomini@studenti.unitn.it>
2018-02-15 14:28:12 +0100

    search API tested

---
+++ tests/search.test.js
@@ -0,0 +1,34 @@
+const fetch = require('node-fetch');
+
+const search = function(word) {
+	return fetch('http://localhost:8080/search/' + word, {
+        method: 'GET',
+        headers: {
+            'Accept': 'application/json'
+        },
+	});
+}
+
+test('Search with a word (sub)contained in almost one tweet', () => {
+	search('uov').then(res => {
+		expect(res.status).toBe(200);
+		return res.json();
+	})
+	.then(r => {
+		expect(r.ok).toBe(true);
+	});
+});
+test('Search with a word not contained in any tweet', () => {
+	search('aa').then(res => {
+		expect(res.status).toBe(200);
+		return res.json();
+	})
+	.then(r => {
+		expect(r.ok).toBe(true);
+	});
+});
+test('Trying to leave the parameter empty', () => {
+	search('').then(res => {
+		expect(res.status).not.toBe(200);
+	});
+});
