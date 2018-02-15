a8f7e61947a5369838f350d6e61b868e1ee7d7ca
Nicolò Pomini <nicolo.pomini@studenti.unitn.it>
2018-02-15 14:11:43 +0100

    fetch API tested

---
+++ tests/fetch.test.js
@@ -0,0 +1,46 @@
+const fetch = require('node-fetch');
+
+const getAll = function() {
+	return fetch('http://localhost:8080/fetch', {
+        method: 'GET',
+        headers: {
+            'Accept': 'application/json'
+        },
+	});
+}
+const getByUser = function(username) {
+	return fetch('http://localhost:8080/fetch/' + username, {
+        method: 'GET',
+        headers: {
+            'Accept': 'application/json'
+        },
+	});
+}
+
+test('Retrieving all tweets', () => {
+	getAll().then(res => {
+		expect(res.status).toBe(200);
+		return res.json();
+	})
+	.then(r => {
+		expect(r.ok).toBe(true);
+	})
+});
+test('Retrieving tweets from a valid username', () => {
+	getByUser('pincopallino').then(res => {
+		expect(res.status).toBe(200);
+		return res.json();
+	})
+	.then(r => {
+		expect(r.ok).toBe(true);
+	})
+});
+test('Retrieving tweets from a non valid username', () => {
+	getByUser('blabla').then(res => {
+		expect(res.status).not.toBe(200);
+		return res.json();
+	})
+	.then(r => {
+		expect(r.ok).toBe(false);
+	})
+});
